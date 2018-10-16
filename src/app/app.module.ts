9f56bc65ed03a1376eac937dc8eb69aec8b5f8a1
AuraMelisa01 <auramelisa6@gmail.com>
2018-10-16 01:49:07 -0500

    Creando maquetacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    SobreMiComponent,
+    PortafolioComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
